734cfa36112fbbd3b871dcaa242f69ed0affe207
kinick <kimjun9809@gmail.com>
2023-10-29 11:44:34 +0900

    학생 데이터 입력 패키지에 저장한 변수 순서대로 자료 입력(서버에서 받아오는것이 맞는 방법)

--- app/src/main/java/com/example/listview_20231029/MainActivity.kt
+++ app/src/main/java/com/example/listview_20231029/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.listview_20231029
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.listview_20231029.adapters.StudentAdapter
+import com.example.listview_20231029.datas.studentData
 
 class MainActivity : AppCompatActivity() {
+
+    val  mStudentList=ArrayList<studentData>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       mStudentList.add(studentData("조철뚝",1999,"010-9999-9999"))
+        mStudentList.add(studentData("김준",2000,"010-9999-9999"))
+        mStudentList.add(studentData("우짤래미",2222,"010-9999-9999"))
+        mStudentList.add(studentData("저짤래미",2313,"010-9999-9999"))
+        mStudentList.add(studentData("굿",4212,"010-9999-9999"))
+        mStudentList.add(studentData("ㅇㅇ",4232,"010-9999-9999"))
+        mStudentList.add(studentData("쥰킴",1254,"010-9999-9999"))
+        mStudentList.add(studentData("쥬시",2000,"010-9999-9999"))
+
+//      어뎁터 변수도 객체생성
+
+
+
     }
 }

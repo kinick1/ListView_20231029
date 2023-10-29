f3e38f0deb4840ce8357605a17cb007ecefd9738
kinick <kimjun9809@gmail.com>
2023-10-29 13:42:54 +0900

    주석 추가

--- app/src/main/java/com/example/listview_20231029/datas/studentData.kt
+++ app/src/main/java/com/example/listview_20231029/datas/studentData.kt
@@ -3,7 +3,7 @@ package com.example.listview_20231029.datas
 import android.util.Log
 import java.time.Year
 
-class studentData (
+class studentData ( // StudentData의 생성자 변경 : 이름, 출생년도, 폰번을 넣어서 생성하도록.
     val name: String,
     val birthYear: Int,
     val phoneNum: String ) {
@@ -13,11 +13,12 @@ class studentData (
 
     constructor( name: String, birthYear: Int ) : this(name, birthYear, "폰번 모름")
 
+    //    현재 한국식 나이를 계산해서 리턴해주는 함수
     fun getKoreanAge(year:Int):Int{
         return year-this.birthYear+1
     }
 
-    fun printSimplePhoneNum(){
+    fun printSimplePhoneNum(){ // 리턴타입 명시 X :  리턴 없다. (JAVA 의 void)
         Log.d("학생데이터", this.phoneNum.replace("-", ""))
     }
 

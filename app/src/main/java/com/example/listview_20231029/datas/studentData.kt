1903bed5de3edb0fb3520113228d67c33bfaf753
kinick <kimjun9809@gmail.com>
2023-10-29 11:19:25 +0900

    학생에 관한 변수들을 한꺼번에 관리할 studentData클래스 생성

---
+++ app/src/main/java/com/example/listview_20231029/datas/studentData.kt
@@ -0,0 +1,7 @@
+package com.example.listview_20231029.datas
+
+import java.time.Year
+
+class studentData (name:String,birthYear: Int,PhoneNum:String){     //StudentData의 생성자 변경: 이름,출생연도, 폰번호를 넣어서 생성하기
+    
+}

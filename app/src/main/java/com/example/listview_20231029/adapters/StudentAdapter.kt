d93f5697507f2c77f482c23696fe5193e5432ebd
kinick <kimjun9809@gmail.com>
2023-10-29 11:35:44 +0900

    학생 데이터 입력 패키지에 저장한 변수 순서대로 자료 입력(서버에서 받아오는것이 맞는 방법)

--- app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
@@ -1,9 +1,28 @@
 package com.example.listview_20231029.adapters
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview_20231029.R
 import com.example.listview_20231029.datas.studentData
 
 class StudentAdapter(mContext:Context, resID:Int,mList:ArrayList<studentData> ):ArrayAdapter<studentData>(mContext,resID,mList) {
 
+//      커스텀으로 만든 xml을 => 코틀린단으로 끌어와주는 객체
+
+    val  inf=LayoutInflater.from(mContext)
+//    완성한 inf를 이용해서 =>xml을 가져와주는 함수
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var  tempRow=convertView
+
+        if(tempRow==null){
+            tempRow=inf.inflate( R.layout.student_list_item,null)
+        }
+        val row=tempRow!!
+
+        return row
+    }
 
 }
